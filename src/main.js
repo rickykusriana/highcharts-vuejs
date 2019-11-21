51b29a0ee360da7aa1c99049661577a2e9c69861
Ricky <rickykusriana@gmail.com>
2019-11-21 12:00:12 +0700

    Starting

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import HighchartsVue from 'highcharts-vue'
+// import Highcharts from 'highcharts'
+// import stockInit from 'highcharts/modules/stock'
+
+// stockInit(Highcharts)
+
+Vue.use(HighchartsVue)
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
